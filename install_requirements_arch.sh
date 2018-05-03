da1a48e13eb05f95ce266ecfb978e6f1ad0d40ec
James Shane <jamesshane@gmail.com>
2018-05-02 22:42:40 -0600

    Modded install_requirements_arch.sh to pull yaourt from aur and install, Modded polybar.template to remove sudo from reboot and power off (user that has sudo doesn't need to authenticate this priv.

--- install_requirements_arch.sh
+++ install_requirements_arch.sh
@@ -2,5 +2,15 @@
 
 ### Script written by Stavros Grigoriou ( github.com/unix121 )
 
-sudo pacman -S git yaourt nitrogen rofi python-pip ttf-font-awesome adobe-source-code-pro-fonts
+sudo pacman -S git nitrogen rofi python-pip ttf-font-awesome adobe-source-code-pro-fonts 
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si --noconfirm
+cd ..
+rm -fr package-query
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si --noconfirm
+cd ..
+rm -fr yaourt
 yaourt -S polybar-git nerd-fonts-complete --noconfirm
