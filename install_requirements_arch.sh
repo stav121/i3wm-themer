a3574046880a9d2c57bb4c8eba9deba00e058224
James Shane <jamesshane@gmail.com>
2018-05-06 21:58:54 -0600

    going with ttf-nerd-fonts-symbols

--- install_requirements_arch.sh
+++ install_requirements_arch.sh
@@ -26,11 +26,11 @@ yaourt -S polybar-git --noconfirm
 
 #directory may not be needed, but it makes a cleaner install, went with nerd-fonts-git, other is outta date
 sudo mkdir /usr/share/fonts/OTF
-#git clone https://aur.archlinux.org/nerd-fonts-git.git
-#cd nerd-fonts-git
-#makepkg -si --noconfirm
-#cd ..
-#rm -fr nerd-fonts-git
+git clone https://aur.archlinux.org/ttf-nerd-fonts-symbols.git 
+cd ttf-nerd-fonts-symbols
+makepkg -si --noconfirm
+cd ..
+rm -fr ttf-nerd-fonts-symbols
 
 #install -Dm644 /usr/share/doc/polybar/config $HOME/.config/polybar/config
 
