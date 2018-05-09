8c6c91a604595501e99ea696f1473b0db4d25862
James Shane <jamesshane@gmail.com>
2018-05-09 00:37:47 -0600

    reworked install to use yaourt tmp flag, remerged setup back to install

--- install_requirements_arch.sh
+++ install_requirements_arch.sh
@@ -5,7 +5,7 @@
 
 
 #added binutils,gcc,make,pkg-config,fakeroot for compilations, removed yaourt
-sudo pacman -S git nitrogen rofi python-pip ttf-font-awesome adobe-source-code-pro-fonts binutils gcc make pkg-config fakeroot
+sudo pacman -S git nitrogen rofi python-pip ttf-font-awesome adobe-source-code-pro-fonts binutils gcc make pkg-config fakeroot --noconfirm
 
 #added PYTHONDONTWRITEBYTECODE to prevent __pycache__
 export PYTHONDONTWRITEBYTECODE=1
@@ -23,15 +23,17 @@ makepkg -si --noconfirm
 cd ..
 rm -fr yaourt
 mkdir $HOME/tmpyaourt
-yaourt -S polybar-git --noconfirm --tmp $HOME/tmpyaourt
+#went with nerd-fonts-git, other is outta date
+yaourt -S polybar-git ttf-nerd-fonts-symbols --noconfirm --tmp $HOME/tmpyaourt
+rmdir $HOME/tmpyaourt
 
 #directory may not be needed, but it makes a cleaner install, went with nerd-fonts-git, other is outta date
-sudo mkdir /usr/share/fonts/OTF
-git clone https://aur.archlinux.org/ttf-nerd-fonts-symbols.git 
-cd ttf-nerd-fonts-symbols
-makepkg -si --noconfirm
-cd ..
-rm -fr ttf-nerd-fonts-symbols
+#sudo mkdir /usr/share/fonts/OTF
+#git clone https://aur.archlinux.org/ttf-nerd-fonts-symbols.git 
+#cd ttf-nerd-fonts-symbols
+#makepkg -si --noconfirm
+#cd ..
+#rm -fr ttf-nerd-fonts-symbols
 
 #install -Dm644 /usr/share/doc/polybar/config $HOME/.config/polybar/config
 
@@ -42,4 +44,26 @@ then
 else
 	touch $HOME/.Xresources
 fi
+
+#rework of user in config.yaml
+cd src
+rm -f config.yaml
+cp defaults/config.yaml .
+sed -i -e "s/USER/$USER/g" config.yaml
+
+#file didn't excist for me, so test and touch
+if [ -e $HOME/.config/polybar/config ]
+then
+        echo "... polybar/config found."
+else
+	mkdir $HOME/.config/polybar
+        touch $HOME/.config/polybar/config
+fi
+
+#backup, configure and set theme to 000
+cp -r ../scripts/* /home/$USER/.config/polybar/
+mkdir $HOME/Backup
+python i3wm-themer.py --config config.yaml --backup $HOME/Backup
+python i3wm-themer.py --config config.yaml --install defaults/
+
 
