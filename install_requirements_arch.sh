816aabefb62c496ae39038cc88160a822960b1ef
James Shane <jamesshane@gmail.com>
2018-05-09 23:23:02 -0600

    20180509 release

--- install_requirements_arch.sh
+++ install_requirements_arch.sh
@@ -3,6 +3,8 @@
 ### Script written by Stavros Grigoriou ( github.com/unix121 )
 ### 20180505 Changes fully commented by James Shane ( github.com/jamesshane )
 
+#refrsh pacman
+sudo pacman -Syy
 
 #added binutils,gcc,make,pkg-config,fakeroot for compilations, removed yaourt
 sudo pacman -S git nitrogen rofi python-pip ttf-font-awesome adobe-source-code-pro-fonts binutils gcc make pkg-config fakeroot --noconfirm
@@ -22,20 +24,11 @@ cd yaourt
 makepkg -si --noconfirm
 cd ..
 rm -fr yaourt
+#tmp dir for yaourt, /tmp may be too small
 mkdir $HOME/tmpyaourt
-#went with nerd-fonts-git, other is outta date
+#went with ttf-nerd-fonts, other is outta date
 yaourt -S polybar-git ttf-nerd-fonts-symbols --noconfirm --tmp $HOME/tmpyaourt
 rmdir $HOME/tmpyaourt
-
-#directory may not be needed, but it makes a cleaner install, went with nerd-fonts-git, other is outta date
-#sudo mkdir /usr/share/fonts/OTF
-#git clone https://aur.archlinux.org/ttf-nerd-fonts-symbols.git 
-#cd ttf-nerd-fonts-symbols
-#makepkg -si --noconfirm
-#cd ..
-#rm -fr ttf-nerd-fonts-symbols
-
-#install -Dm644 /usr/share/doc/polybar/config $HOME/.config/polybar/config
 
 #file didn't exist for me, so test and touch
 if [ -e $HOME/.Xresources ]
@@ -66,4 +59,6 @@ mkdir $HOME/Backup
 python i3wm-themer.py --config config.yaml --backup $HOME/Backup
 python i3wm-themer.py --config config.yaml --install defaults/
 
+echo ""
+echo "Read the README.md"
 
