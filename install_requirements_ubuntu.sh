898cf2eabaf6b7dcf7b08ef2d5bd07b699043ac1
James Shane <jamesshane@gmail.com>
2018-05-25 04:02:27 +0000

    Added i3-gaps to main

--- install_requirements_ubuntu.sh
+++ install_requirements_ubuntu.sh
@@ -1,13 +1,49 @@
 #!/usr/bin/env sh
 
-### 201805 Script written and fully commented by James Shane ( github.com/jamesshane )
+### 20180515 Script written and fully commented by James Shane ( github.com/jamesshane )
 
-#refrsh apt
+#look for env command and link if not found to help make scripts uniform
+if [ -e /bin/env ]
+then
+	echo "... /bin/env found."
+else
+	sudo ln -s /usr/bin/env /bin/env
+fi
+
+#refresh apt
 sudo apt update
 
-#adobe-source-code-pro-fonts **
+sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev dh-autoreconf
+
+git clone --recursive https://github.com/Airblader/xcb-util-xrm.git
+cd xcb-util-xrm/
+./autogen.sh
+make
+sudo make install
+cd ..
+
+#cat > /etc/ld.so.conf.d/i3.conf
+#/usr/local/lib/
+
+sudo ldconfig
+sudo ldconfig -p
+
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+autoreconf --force --install
+rm -Rf build/
+mkdir build
+cd build/
+ ../configure --prefix=/usr --sysconfdir=/etc
+ make
+ sudo make install
+# which i3
+# ls -l /usr/bin/i3
+cd ../..
+
+
 #added binutils,gcc,make,pkg-config,fakeroot for compilations, removed yaourt
-sudo apt install git nitrogen rofi python-pip binutils gcc make pkg-config fakeroot cmake python-xcbgen xcb-proto libxcb-ewmh-dev wireless-tools -y
+sudo apt install git nitrogen rofi python-pip binutils gcc make pkg-config fakeroot cmake python-xcbgen xcb-proto libxcb-ewmh-dev wireless-tools libiw-dev -y
 
 #sudo apt-get install libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev -y 
 
@@ -25,9 +61,6 @@ cd fontawesome-free-5.0.13
 sudo cp use-on-desktop/* /usr/share/fonts
 sudo fc-cache -f -v
 cd ../..
-
-#wget http://mirrors.kernel.org/ubuntu/pool/main/f/fonts-font-awesome/fonts-font-awesome_4.7.0~dfsg-3_all.deb
-#sudo apt install -f ./fonts-font-awesome_4.7.0~dfsg-3_all.deb
 
 git clone https://github.com/jaagr/polybar
 cd polybar
@@ -60,7 +93,7 @@ else
         touch $HOME/.config/polybar/config
 fi
 
-#backup, configure and set theme to 000
+#backup, configure and set theme to default
 cp -r ../scripts/* /home/$USER/.config/polybar/
 mkdir $HOME/Backup
 python i3wm-themer.py --config config.yaml --backup $HOME/Backup
