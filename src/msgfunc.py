663bc97ddd54e606f1465ee9f21cf79862e0f1f6
James Shane <jamesshane@gmail.com>
2018-06-10 12:24:37 -0600

    added new msging colors for all os's

--- src/msgfunc.py
+++ src/msgfunc.py
@@ -4,13 +4,28 @@ def prnt( arg, msg ):
     prefix_fail = '\x1b[0;31;40m [x] \x1b[0m'
     prefix_fatal = '\x1b[0;31;40m [!] '
     prefix_normal = '\x1b[0m'
+
+    class bcolors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+
     #print ("arg=",arg," msg=",msg)
     if arg == '-s':
-        print( prefix_success, msg)
+        print (bcolors.OKGREEN + "[>] " + bcolors.ENDC + msg)
+        #print( prefix_success, msg)
     elif arg == '-f':
-        print( prefix_fail, msg)
+        print (bcolors.FAIL + "[x] " + bcolors.ENDC + msg)
+        #print( prefix_fail, msg)
     elif arg == '-n':
-        print( prefix_blue, msg, prefix_normal)
+        print (bcolors.OKBLUE + "[>] " + bcolors.ENDC + msg)
+        #print( prefix_blue, msg, prefix_normal)
     else:
-        print( prefix_fatal, 'Wrong argument given in prnt() function!', prefix_normal)
+        print (bcolors.WARNING + "[!] " + bcolors.ENDC + msg)
+        #print( prefix_fatal, 'Wrong argument given in prnt() function!', prefix_normal)
         exit(9)
