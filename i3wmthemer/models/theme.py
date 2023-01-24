7a534bf1b87f46d9eb5252c35e02b1bc89bee76a
alex <alex.palermo.45@gmail.com>
2023-01-24 18:24:36 -0500

    started setting up script to automatically generate hex codes for xresources from pywal

--- i3wmthemer/models/theme.py
+++ i3wmthemer/models/theme.py
@@ -4,6 +4,7 @@ from i3wmthemer.models.nitrogen import NitrogenTheme
 #from i3wmthemer.models.polybar import PolybarTheme
 from i3wmthemer.models.status import StatusbarTheme
 from i3wmthemer.models.xresources import XresourcesTheme
+import pywal
 
 
 class Theme(AbstractTheme):
@@ -17,12 +18,13 @@ class Theme(AbstractTheme):
 
         :param file: the JSON file to load from.
         """
-
+        if 'settings': in file:
+            file = self.parse_settings(file)
         if 'xresources' in file:
             self.x_resources = XresourcesTheme(file)
         self.i3_theme = I3Theme(file)
         self.statusbar_theme = StatusbarTheme(file)
-        #self.polybar_theme = PolybarTheme(file)
+        self.polybar_theme = PolybarTheme(file)
         self.nitrogen_theme = NitrogenTheme(file)
 
     def load(self, configuration):
@@ -36,3 +38,21 @@ class Theme(AbstractTheme):
         self.polybar_theme.load(configuration)
         self.nitrogen_theme.load(configuration)
         configuration.refresh_all(self.nitrogen_theme.wallpaper)
+
+    def parse_settings(self, file):
+        if 'use_pywal' in file['settings'] and file['use_pywal']:
+            file = self.populate_file_from_pywal(file)
+
+
+    def populate_file_from_pywal(self, file):
+        wallpaper = file['wallpaper']
+        colors = pywal.colors.get(wallpaper)
+
+        ### xresources
+        for key in colors['colors']:
+            file['xresources'][key] = colors['colors'][key]
+        file['xresources']['background'] = colors['special']['background']
+        file['xresources']['foreground'] = colors['special']['foreground']
+        file['xresources']['cursorcolor'] = colors['special']['cursor']
+
+
